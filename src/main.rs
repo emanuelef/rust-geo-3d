7ae4d0c3efeee6f90b27a284a8d034ec5f92318d
Emanuele Fumagalli <efumagal@synamedia.com>
2023-12-24 21:07:42 +0000

    work in progress

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
+use crate::position::haversine_distance;
 mod point3d;
+mod position;
 
 fn main() {
     let p1 = point3d::Point3D { x: 1.0, y: 2.0, z: 3.0 };
     let p2 = point3d::Point3D { x: 4.0, y: 5.0, z: 6.0 };
     let p3 = p1.add(p2);
     print!("p3 = {:?}\n", p3);
+
+    let position1 = position::Coord3D {
+        coord2d: position::Coord2D { lat: 1.0, lon: 2.0 },
+        alt: 3.0,
+    };  
+
+    let position2 = position::Coord3D {
+        coord2d: position::Coord2D { lat: 3.0, lon: 7.0 },
+        alt: 6.0,
+    };  
+
+    print!("position1 = {:?}\n", position1);
+
+    let distance = haversine_distance(&position1.coord2d, &position2.coord2d);
+
+    print!("distance = {:?}\n", distance);
 }
--- src/position.rs
+++ src/position.rs
@@ -1,22 +1,23 @@
 use std::f64;
 
-const WGS84_a: f64 = 6378137.0;
-const WGS84_b: f64 = 6356752.314245;
-const WGS84_f: f64 = 1 / 298.257223563;
+const WGS84_A: f64 = 6378137.0;
+const WGS84_B: f64 = 6356752.314245;
+const WGS84_F: f64 = 1.0 / 298.257223563;
 
-struct Coord2D {
-    lat: f64,
-    lon: f64,
+#[derive(Debug)]
+pub(crate) struct Coord2D {
+    pub lat: f64,
+    pub lon: f64,
+}
+#[derive(Debug)]
+pub(crate) struct Coord3D {
+    pub coord2d: Coord2D,
+    pub alt: f64,
 }
 
-struct Coord3D {
-    coord2d: Coord2D,
-    alt: f64,
-}
-
-struct Coord4D {
-    coord3d: Coord3D,
-    timestamp: i64,
+pub(crate) struct Coord4D {
+    pub coord3d: Coord3D,
+    pub timestamp: i64,
 }
 
 fn new_coord3d(lat: f64, lon: f64, alt: f64) -> Coord3D {
@@ -37,20 +38,20 @@ fn hsin(theta: f64) -> f64 {
     theta * theta / 2.0
 }
 
-fn haversine_distance(p1: Coord2D, p2: Coord2D) -> f64 {
+pub(crate) fn haversine_distance(p1: &Coord2D, p2: &Coord2D) -> f64 {
     let pi_rad = std::f64::consts::PI / 180.0;
     let a = p1.lat * pi_rad;
     let b = p2.lat * pi_rad;
-    let c = (p1.lon - p2.lon) * pi_rad * WGS84_f;
-    let d = hsin(b - a) + math::cos(a) * math::cos(b) * hsin(c);
+    let c = (p1.lon - p2.lon) * pi_rad * WGS84_F;
+    let d = hsin(b - a) + f64::cos(a) * f64::cos(b) * hsin(c);
 
-    let meters = 2.0 * WGS84_a * math::asin(math::sqrt(d)) as f64;
+    let meters = 2.0 * WGS84_A * f64::asin(f64::sqrt(d)) as f64;
 
     meters
 }
 
 fn distance3d(a: Coord3D, b: Coord3D) -> f64 {
-    let distance2d = haversine_distance(a.coord2d, b.coord2d);
+    let distance2d = haversine_distance(&a.coord2d, &b.coord2d);
     let h = a.alt - b.alt;
 
     let distance_: f64 = ((distance2d.powi(2) + h.powi(2)) as f64).sqrt();
