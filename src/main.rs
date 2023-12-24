6a9846e8d5ed5f1e20ce75123289b1bc2f0c1a0f
Emanuele Fumagalli <efumagal@synamedia.com>
2023-12-24 15:21:41 +0000

    working on main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod point3d;
+
 fn main() {
-    println!("Hello, world!");
+    let p1 = point3d::Point3D { x: 1.0, y: 2.0, z: 3.0 };
+    let p2 = point3d::Point3D { x: 4.0, y: 5.0, z: 6.0 };
+    let p3 = p1.add(p2);
+    print!("p3 = {:?}\n", p3);
 }
--- src/point3d.rs
+++ src/point3d.rs
@@ -1,7 +1,9 @@
+#[derive(Debug)]
+
 pub struct Point3D {
-	x: f64,
-	y: f64,
-	z: f64,
+	pub(crate) x: f64,
+	pub(crate) y: f64,
+	pub(crate) z: f64,
 }
 
 impl Point3D {
@@ -34,11 +36,14 @@ impl Point3D {
 	}
 
 	pub fn distance(&self, other: Point3D) -> f64 {
-		use std::f64::sqrt;
-		sqrt(square(self.x - other.x) + square(self.y - other.y) + square(self.z - other.z))
-	}
+        let dx = self.x - other.x;
+        let dy = self.y - other.y;
+        let dz = self.z - other.z;
+        let len = f64::sqrt(dx * dx + dy * dy + dz * dz);
+        if len == 0.0 {
+            return 0.0;
+        } else {
+            return len;
+        }	}
 
-	fn square(&self, x: f64) -> f64 {
-		x * x
-	}
 }
